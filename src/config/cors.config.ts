b94110228eaa11d6eea8ec9383b560e95038d27b
jgrove <26723005+jgrove2@users.noreply.github.com>
2024-11-12 00:29:32 +0000

    cors

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,5 +1,5 @@
 const corsConfig = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://marmot-worker.justin-grove42.workers.dev/"],
   allowMethods: ["POST", "GET", "DELETE"],
 };
 

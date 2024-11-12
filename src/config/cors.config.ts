c4ef252c6e6855a00e9043e726c4cc136c05ed10
jgrove <26723005+jgrove2@users.noreply.github.com>
2024-11-12 00:31:59 +0000

    cors

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,6 +1,9 @@
 const corsConfig = {
   origin: ["http://localhost:5173", "https://marmot-worker.justin-grove42.workers.dev/"],
   allowMethods: ["POST", "GET", "DELETE"],
+  allowHeaders: ['Content-Type', 'Authorization'],
+  maxAge: 600,
+  credentials: true
 };
 
 export default corsConfig;
